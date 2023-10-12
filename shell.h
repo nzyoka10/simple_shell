a82aa6d1cf31441d817c928342480bdca021ec75
root <mnzyokaeric@gmail.com>
2023-10-12 04:00:19 -0700

    header file

--- shell.h
+++ shell.h
@@ -7,7 +7,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-ssize_t _getline(char **lineptr, size_t *n, FILE *strem);
-void execute_command(char *command);
+void print_prompt(void);
+ssize_t get_input(char **buffer, size_t *bufsize);
+void handle_input_error(char *buffer);
+pid_t fork_and_execute(char *buffer);
+void wait_for_child(void);
 
 #endif /* SHELL_H */
