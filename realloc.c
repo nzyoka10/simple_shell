64a23f2484cda9ef445937afa1b102270c16e2f9
root <mnzyokaeric@gmail.com>
2023-10-14 01:28:42 -0700

    shell files

---
+++ realloc.c
@@ -0,0 +1,35 @@
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ *
+ * Return: pointer to the reallocated block or NULL on failure
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+    char *p;
+
+    if (!ptr)
+        return malloc(new_size);
+
+    if (new_size == 0)
+    {
+        free(ptr);
+        return NULL;
+    }
+
+    if (new_size == old_size)
+        return ptr;
+
+    p = malloc(new_size);
+    if (!p)
+        return NULL;
+
+    old_size = old_size < new_size ? old_size : new_size;
+    while (old_size--)
+        p[old_size] = ((char *)ptr)[old_size];
+
+    free(ptr);
+    return p;
+}
